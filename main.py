e39dc0371d3c7ec0d1bc2bcd40531720fd7ba79d
Aleksander <alexlik73@gmail.com>
2022-04-02 14:26:59 +0300

    add shapes for click

--- main.py
+++ main.py
@@ -35,17 +35,73 @@ def clear_window():
 		t.hideturtle()
 		t.setposition(9999, 9999)
 
-def click(x,y):
-	clicker.setposition(x,y)
+def draw_playing_field():
+	drawer.setposition(0,0)
+	drawer.showturtle()
+	drawer.shape('square')
+	drawer.shapesize(30)
+	drawer.stamp()
+	drawer.setposition(9999,9999)
+	x = -220
+	y = 220
+	for button_row in buttons_game:
+		for button in button_row:
+			button.showturtle()
+			button.color('white')
+			button.shape('square')
+			button.shapesize(10)
+			button.setposition(x, y)
+			x += 220
+		y -= 220
+		x = -220
+
+def menu():
 	if iscollision(clicker, button_single, 20, 60, 10, 10):
 		clear_window()
+		draw_playing_field()
 	elif iscollision(clicker, button_multiplayer, 20, 60, 10, 10):
 		clear_window()
+		draw_playing_field()
+
+def choice_shape():
+	if clicker.clicks%2 == 0:
+		shape = 'x.gif'
+	else:	
+		shape = 'o.gif'
+	return shape
+
+def click(x,y):
+	clicker.setposition(x,y)
+	menu()
+	for button_row in buttons_game:
+		for button in button_row:
+			if iscollision(clicker, button, 90, 10, 90, 10):
+				clicker.clicks += 1
+				shape = choice_shape()
+				button.stamp()
+				button.value = shape
+				button.shape(button.value)
+				button.stamp()
+				button.setposition(9999, 9999)
+	
 
 window = turtle.Screen()
+window.register_shape('x.gif')
+window.register_shape('o.gif')
 button_single = create_object()
 button_multiplayer = create_object()
 clicker = create_object()
+clicker.clicks = 0
+drawer = create_object()
+buttons_game = []
+for row in range(3):
+	row_buttons = []
+	for col in range(3):
+		b = create_object()
+		b.value = ''
+		row_buttons.append(b)
+	buttons_game.append(row_buttons) 
+
 start_game()
 window.onclick(click)
 turtle.done()
