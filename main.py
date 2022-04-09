8f31d6f3139ad4004ab22362b02b86380fcb5379
Aleksander <alexlik73@gmail.com>
2022-04-09 14:30:46 +0300

    add is_solved

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import turtle
 
+def is_solved(board):
+    for i in range(3):
+        if board[0][0] != 0 and board[1][1] != 0:
+            if board[i][0] == board[i][1] == board[i][2]:
+                return board[i][0]
+            if board[0][i] == board[1][i] == board[2][i]:
+                return board[0][i]
+            
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
+    
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                return -1
+    return 0
+
+
 def create_object():
 	t = turtle.Turtle()
 	t.speed(0)
